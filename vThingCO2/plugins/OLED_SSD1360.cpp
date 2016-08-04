334742e4d9a0136a4d972aed725e153b73ac3a38
Vladi Vladi <vlast3k@gmail.com>
2016-08-04 07:18:59 +0300

    OLED fixed mem leak

--- vThingCO2/plugins/OLED_SSD1360.cpp
+++ vThingCO2/plugins/OLED_SSD1360.cpp
@@ -5,15 +5,13 @@
 SSD1306  *display = NULL;
 
 void oledHandleCommand(char *cmd) {
-  Serial <<"1\n";
   if (!hasSSD1306)  return;
-  Serial <<"2\n";
-  if (!display) display = new SSD1306(0x3c, D5, D1);
-  Serial <<"3\n";
-
-  display->init();
-  display->flipScreenVertically();
-  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  if (!display) {
+     display = new SSD1306(0x3c, D5, D1);
+     display->init();
+     display->flipScreenVertically();
+     display->setTextAlignment(TEXT_ALIGN_LEFT);
+   }
   if (strlen(cmd) < 8) {
     display->setFont(ArialMT_Plain_24);
   } else {
@@ -21,7 +19,5 @@ void oledHandleCommand(char *cmd) {
   }
   display->drawString(0, 0, cmd);
   display->display();
-  Serial <<"4\n";
-
 }
 #endif
