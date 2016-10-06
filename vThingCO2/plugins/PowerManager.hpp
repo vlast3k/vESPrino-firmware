ae6a4f21e171d56dbfa04dc9e1b42a24dc3dd1fe
Vladimir Savchenko <vladimir.savchenko@sap.com>
2016-10-06 17:16:17 +0300

    set interval to 30sec

--- vThingCO2/plugins/PowerManager.hpp
+++ vThingCO2/plugins/PowerManager.hpp
@@ -20,7 +20,7 @@ public:
   const char* getName() {
     return "PowerManager";
   }
-  const static uint8_t IterationDurationS = 20;
+  const static uint8_t IterationDurationS = 30;
   void static cmdDeepSleep(const char *line) ;
   void cmdDeepSleepInst(const char *line) ;
   bool isWokeFromDeepSleep();
